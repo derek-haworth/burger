ca7a95ba3833ded7de5a6fa93cad4ec27a2e9558
Derek Haworth <derek_haworth@bradycorp.com>
2018-03-06 17:39:55 -0600

    update schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,16 @@
 -- ***********************************************************************************
 
 /* Create and use the burgers_db */
-CREATE DATABASE  `burgers_db`;
-USE `burgers_db`;
+CREATE DATABASE  burgers_db;
+USE burgers_db;
 
 /* Create a table for all your burgers */
-CREATE TABLE `burgers` (
-	`id` INTEGER(11) AUTO_INCREMENT NOT NULL,
-	`burger_name` VARCHAR(255) NOT NULL,
-	`name` BOOLEAN DEFAULT true,
-	/* Set ID as primary key */
-	PRIMARY KEY (`id`) 
+CREATE TABLE burgers (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN DEFAULT true,
+	PRIMARY KEY (id)
 ); 
+
+
+SELECT * FROM burgers;
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name) 
+VALUES ("Big Mac Burger"),("Whopper"),("Breakfast Burger");
