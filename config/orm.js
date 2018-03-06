f93a58517354a86a261dbf77cdfbae37a02e4ead
derek-haworth <derekhaw1@gmail.com>
2018-03-05 23:27:36 -0600

    set functions in orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,14 @@
 // *********************************************************************************
 // File offers a set of easier-to-use methods for interacting with the MySQL db.
 // *********************************************************************************
+/*
+* In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+
+     * `selectAll()`
+     * `insertOne()`
+     * `updateOne()`
+*/
+
 
 // Dependencies
 // =============================================================
@@ -12,7 +20,14 @@ var connection = require("./connection.js");
 var tableName = "burgers";
 
 var orm = {
-  
-};
+	selectAll: function() {
+		
+	},
+	insertOne: function() {
+		
+	},
+	updateOne: function() {
+	}
+}
 
 module.exports = orm;
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require("body-parser");
 
-
 // Sets up the Express App
 // =============================================================
 var app = express();
