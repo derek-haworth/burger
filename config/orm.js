5f844830499b07886b5188662fe90769d910c689
derek-haworth <derekhaw1@gmail.com>
2018-03-05 23:31:52 -0600

    add orm sql helper function

--- config/orm.js
+++ config/orm.js
@@ -17,7 +17,28 @@ var connection = require("./connection.js");
 // ORM
 // =============================================================
 
-var tableName = "burgers";
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(obj) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in obj) {
+    var value = obj[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(obj, key)) {
+      // if string with spaces, add quotations (Big Mac => 'Big Mac')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // ex: {name: 'The Best Burger'} => ["name='The Best Burger'"]
+      // ex: {devoured: true} => ["devoured=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
 
 var orm = {
 	selectAll: function() {
