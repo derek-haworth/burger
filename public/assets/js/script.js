f9fe4c55781ffef67115cbfce93e53d3e1457a54
Derek Haworth <derek_haworth@bradycorp.com>
2018-03-09 15:26:53 -0600

    add error-handling if input is blank

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,13 +1,21 @@
 $(function() {
+	$('.input-error').hide();
+
 	// Submit Button - Add A New Burger
 	$("#btnSubmit").on("click", function(event) {
 		event.preventDefault();
 		console.log("Submit Button was pressed.");
+		let burgerName = $("#burgerName").val().trim()
+		if (burgerName == "") {
+			$('.input-error').show();
+			return;
+		}
+
 		// Insert new burger name to the burger_name in the burgers table
 		$.ajax("/api/burgers", {
 			type: "POST",
 			data: { 
-				burgerName: $("#burgerName").val().trim() 
+				burgerName: burgerName 
 			}
 		}).then(
 			function() {
