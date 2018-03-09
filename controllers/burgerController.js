ef83c20aaf35db97f7e1b72ee118036baa063a68
Derek Haworth <derek_haworth@bradycorp.com>
2018-03-09 11:46:35 -0600

    add put and post routes in controllers

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -6,7 +6,33 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  
+	burger.selectAll(function(data) {
+      var burgerObject = {
+        burgers: data
+      };
+      console.log(burgerObject);
+      res.render("index", burgerObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne(req.body.burgerName, function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id=" + req.params.id;
+
+    burger.updateOne({ devoured: '1' }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
 });
 
 
